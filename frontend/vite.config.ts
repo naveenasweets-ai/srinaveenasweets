c5ae4d4d69b197c400b1b46ac361536f73165843
vamshi7941 <vamshidhardawoor644@gmail.com>
2026-07-09 16:29:23 +0530

    warning fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/srinaveenasweets/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
